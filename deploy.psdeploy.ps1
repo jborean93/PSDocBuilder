5a1f52e7d533a1e2f9bafdcb4f0102483f71a526
Jordan Borean <jborean93@gmail.com>
2019-06-08 09:02:38 +1000

    Test out why that didn't work

--- deploy.psdeploy.ps1
+++ deploy.psdeploy.ps1
@@ -1,12 +1,15 @@
 # Copyright: (c) 2019, Jordan Borean (@jborean93) <jborean93@gmail.com>
 # MIT License (see LICENSE or https://opensource.org/licenses/MIT)
 
+$ErrorActionPreference = 'Stop'
+
 $module_name = (Get-ChildItem -Path ([System.IO.Path]::Combine($DeploymentRoot, 'Build', '*', '*.psd1'))).BaseName
 $source_path = [System.IO.Path]::Combine($DeploymentRoot, 'Build', $module_name)
 
 $nupkg_version = $env:APPVEYOR_BUILD_VERSION
 if ($env:APPVEYOR_REPO_TAG) {
     $nupkg_version = $env:APPVEYOR_REPO_TAG_NAME
+    throw "$($env:APPVEYOR_BUILD_VERSION) - $($env:APPVEYOR_REPO_TAG) - $($env:APPVEYOR_REPO_TAG_NAME) - $nupkg_version"
 }
 
 Deploy Module {
